bc240b90802301bedf321657f12551b0d35da874
kfran <117746405+kfran697@users.noreply.github.com>
2024-11-21 16:26:03 +0100

    Add files via upload

---
+++ Prática funciones/prática1/act1.js
@@ -0,0 +1,14 @@
+let palabras = [];
+let palabra;
+
+while ((palabra = prompt('Introduzca una palabra')) !== null && palabra !== '') {
+    palabras.push(palabra);
+}
+
+let palabrasUnicas = [...new Set(palabras)];
+palabrasUnicas.sort((a, b) => b.localeCompare(a));
+
+let zonaPalabras = document.getElementById('palabras');
+zonaPalabras.innerHTML = palabrasUnicas.join("<br>");
+
+console.log(palabrasUnicas);
