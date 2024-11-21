b20166f670a2693a0c2c67d18ecb29ba95deca84
kfran697 <kfran697@gmail.com>
2024-11-21 16:33:17 +0100

    cambios hechos

--- Prática funciones/prática1/act1.js
+++ Prática funciones/prática1/act1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let palabras = [];
 let palabra;
 
@@ -11,20 +10,3 @@ palabrasUnicas.sort((a, b) => b.localeCompare(a));
 
 let zonaPalabras = document.getElementById('palabras');
 zonaPalabras.innerHTML = palabrasUnicas.join("<br>");
-
-=======
-let palabras = [];
-let palabra;
-
-while ((palabra = prompt('Introduzca una palabra')) !== null && palabra !== '') {
-    palabras.push(palabra);
-}
-
-let palabrasUnicas = [...new Set(palabras)];
-palabrasUnicas.sort((a, b) => b.localeCompare(a));
-
-let zonaPalabras = document.getElementById('palabras');
-zonaPalabras.innerHTML = palabrasUnicas.join("<br>");
-
->>>>>>> bc240b90802301bedf321657f12551b0d35da874
-console.log(palabrasUnicas);
